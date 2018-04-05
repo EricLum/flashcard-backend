bb3e3c26930795cc365e0c17613d75a0e8de5ad7
eal6gf <elum1234@gmail.com>
2018-04-05 19:46:02 -0400

    first commit

---
+++ db/migrate/20180405233411_create_deck_cards.rb
@@ -0,0 +1,9 @@
+class CreateDeckCards < ActiveRecord::Migration[5.1]
+  def change
+    create_table :deck_cards do |t|
+      t.integer :deck_id
+      t.integer :card_id
+      t.timestamps
+    end
+  end
+end
